7b386f8f6b20da0d55fe6aa1e9a28615cf0e351e
upi1682 <upi538287@gmail.com>
2026-02-23 18:54:43 +0700

    mengaktofkan perintah output input

--- pertemuan2.cpp
+++ pertemuan2.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 using namespace std;
 
-   //variable global
-int sks;
 
-int main () {
-    // variable lokal 
- 
-    int umur;
-    umur = 20;
 
-    cout << "Hallo Mas Abra" << endl;
-    cout << 10 << endl;
-    cout << umur;
-
-}
